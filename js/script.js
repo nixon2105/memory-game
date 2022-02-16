3a524ba0f5946b1e4a14190337532b9684ce57b7
nixon2105 <radivilov1991@mail.ru>
2022-02-16 10:25:08 +0300

    feat: add flip cards

--- js/script.js
+++ js/script.js
@@ -3,11 +3,9 @@ const container = document.querySelector('.cards');
 
 const createTemplate = (data) => {
   return `<div class="card">
-            <div class="card__font">
-              <img src="../assets/images/cards/bg-cards.png" alt="logo" />
-            </div>
-            <div class="card__back">
-              <img src="${data.image}" alt="logo" />
+            <div class="card__inner">
+              <div class="card__font"></div>
+              <div class="card__back" style='background-image: url("${data.image}");'></div>
             </div>
           </div>`;
 };
@@ -29,10 +27,20 @@ const showItem = (data) => {
 
 getData();
 
-const flip = (e) => {
-  const card = e.target.closest('.card');
-  if (e.target.className.includes('card__font')) {
-    card.classList.add('is-flipped');
+container.addEventListener('click', (e) => {
+  const target = e.target;
+  const card = target.closest('.card');
+  if (card) {
+    card.classList.add('flip');
   }
-};
-container.addEventListener('click', flip);
+});
+
+// const flip = (e) => {
+//   // const card = e.target.closest('.card');
+//   if (e.target.className.includes('card__font')) {
+//     // card.classList.add('is-flipped');
+//     console.log('hello');
+//     1;
+//   }
+// };
+// cardsContainer.addEventListener('click', flip);
